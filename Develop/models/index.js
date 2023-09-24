6be5bb83577125a138628bedb9118bab238044b0
Nehaps29 <133929172+Nehaps29@users.noreply.github.com>
2023-09-24 16:24:44 -0400

    defined relationship among the tables

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,11 +6,48 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+/*// Define a Driver as having one License to create a foreign key in the `license` table
+Driver.hasOne(License, {
+  foreignKey: 'driver_id',
+  // When we delete a Driver, make sure to also delete the associated License.
+  onDelete: 'CASCADE',
+});
+
+// We can also define the association starting with License
+License.belongsTo(Driver, {
+  foreignKey: 'driver_id',
+}); */
+Product.belongsTo(Category,{
+  foreignKey: 'customer_products'
+});
 // Categories have many Products
+Product.belongsTo(Category,{
+  foreignKey: 'product_customers'
+});
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'product_tag'
+});
+
+
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'tag_product'
+});
 
 module.exports = {
   Product,
